bf2d62754be22e16b76a24437ccded5a5ed12b9d
JujuProdGames <63123247+speedcuber25@users.noreply.github.com>
2022-06-18 14:31:41 -0500

    6-18-22 Player Movement!
    into
    
    THIS IS SO COOL. pog WOOOO ASOUIDHLVD YAUIOWS DGYV DSULOTDV KFUOLY VDALUDUFALESL yay

---
+++ Backrooms 2D/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	private Vector2 movement
+	{
+		get
+		{
+			Vector3 moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+			return moveVector.normalized;
+		}
+	}
+	[SerializeField] private float speed = 5;
+
+	private void Awake()
+	{
+		rb = FindObjectOfType<Rigidbody2D>();
+	}
+
+
+	private void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+	}
+}
